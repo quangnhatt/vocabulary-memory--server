fd76f93feedde0662310728625528ddf4d0a7092
Quang Nhat <quangnhat@Mac.lan>
2026-01-14 19:56:32 +0700

    Add console log dictionary

--- controllers/dictionary.controller.js
+++ controllers/dictionary.controller.js
@@ -1,14 +1,10 @@
 import DictionaryService from "../services/dictionary.service.js";
-import {askGPT} from "../helpers/gpt.helper.js";
-import CONSTANTS from "../common/constants.js";
 
 class DictionaryController {
   async getDictionary(req, res) {
     const phrase = req.query.word || "";
     const result = await DictionaryService.getDictionary(phrase);
-    if (!result || !result.success){
-      const content = await askGPT({type: CONSTANTS.PROMPT_TYPES.DICTIONARY, prompt: phrase});
-    }
+    console.log(result);
     res.json(result);
   }
 }
